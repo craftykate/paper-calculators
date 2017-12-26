ffae5f7965dc5a0ab4e7aa31fe5fbfc9c6bf1e21
Kate McFaul <katemcfaul@gmail.com>
2017-12-26 08:51:09 -0800

    style header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <h1>Kate's Paper-Punch Calculators</h1>
+        <h1>Kate's Paper Calculators</h1>
         <ul>
           <li><NavLink exact to="/">Home</NavLink></li>
           <li><NavLink to="/bag">Gift Bag</NavLink></li>
