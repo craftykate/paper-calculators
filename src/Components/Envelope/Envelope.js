bb2f5631b63bd32d4b6a039ab35049337c0f9bef
Kate McFaul <katemcfaul@gmail.com>
2017-12-26 15:00:41 -0800

    create envelope comp

---
+++ src/Components/Envelope/Envelope.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Envelope.css';
+
+
+class Envelope extends Component {
+
+  render() {
+    return (
+      <p>Envelope</p>
+    );
+  }
+}
+
+export default Envelope;
