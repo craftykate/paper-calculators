cc9536d313832d7a7e778442ac39f1216d6daf96
Kate McFaul <katemcfaul@gmail.com>
2017-12-26 14:16:32 -0800

    finish bag instructions

--- src/Components/Bag/Bag.js
+++ src/Components/Bag/Bag.js
@@ -6,10 +6,11 @@ class Bag extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      height: '5',
-      width: '4',
-      depth: '2',
+      height: '',
+      width: '',
+      depth: '',
       showBagInstruction: false,
+      errorMessage: null,
       bagDirections: ''
     }
   }
@@ -25,16 +26,38 @@ class Bag extends Component {
     }
   }
 
+  reset = () => {
+    this.setState({
+      height: '',
+      width: '',
+      depth: '',
+      errorMessage: null
+    })
+  }
+
+  checkInputs = () => {
+    if (this.state.height && this.state.width && this.state.depth) {
+      this.calculateBag();
+    } else {
+      this.setState({
+        errorMessage: "You must enter numbers for each dimension",
+        showBagInstruction: false
+      })
+    }
+  }
+
   calculateBag = () => {
     const height = Number(this.state.height);
     const width = Number(this.state.width);
     const depth = Number(this.state.depth);
 
-    const paperHeight = (1 + height + (depth / 2) + 0.25);
-    const paperWidth = (0.5 + (depth * 2) + (width * 2));
-    const twoPapers = (((paperWidth - 0.5) / 2) + 0.5);
-    const bottomUp = (depth + 0.25);
-    const sidesIn = (depth / 2);
+    let paperHeight = (1 + height + (depth / 2) + 0.25);
+    let paperWidth = (0.5 + (depth * 2) + (width * 2));
+    const twoPapers = (((paperWidth - 0.5) / 2) + 0.5).toFixed(2);
+    const bottomUp = (depth + 0.25).toFixed(2);
+    const sidesIn = (depth / 2).toFixed(2);
+    paperHeight = paperHeight.toFixed(2);
+    paperWidth = paperWidth.toFixed(2);
 
     const directions = (
       <div className="instructions">
@@ -44,13 +67,13 @@ class Bag extends Component {
             Cut paper <strong>{paperHeight}" tall x {paperWidth}" wide</strong>.
             <span className="moreInfo">If your paper isnt wide enough you can use two pieces of paper one on top of the other. In that case, each paper would need to be <strong>{twoPapers+`\u0022`} wide</strong> (height is the same).</span>
           </li>
-          <li>Fold either left or right side in 1/2", fold opposite side in to meet the crease so it tucks under flap snugly. (If using two pieces of paper, fold left side of one paper and right side of the other paper in 1/2{`\u0022`}.) Unfold all folds.</li>
-          <li>Fold top down 1" then unfold.</li>
+          <li>Fold either left or right side in 1/2{'"'}, fold opposite side in to meet the crease so it tucks under flap snugly. (If using two pieces of paper, fold left side of one paper and right side of the other paper in 1/2{`\u0022`}.) Unfold all folds.</li>
+          <li>Fold top down 1{'"'} then unfold.</li>
           <li>Carefully apply glue to inside of flap(s), fold top down again (being careful to leave open on glue end) and tuck other side into flap and press down, making sure paper fits snugly up against crease.</li>
-          <li>Fold bottom up {bottomUp}".</li>
+          <li>Fold bottom up {bottomUp}{'"'}.</li>
           <li>Pinch sides of bottom together so side creases are in line with bottom crease.</li>
-          <li>Fold bottom and top edges towards middle crease, overlapping middle crease by 1/4". Glue overlap.</li>
-          <li>Fold sides in {sidesIn}"</li>
+          <li>Fold bottom and top edges towards middle crease, overlapping middle crease by 1/4{'"'}. Glue overlap.</li>
+          <li>Fold sides in {sidesIn}{'"'}</li>
           <li>Decorate bag if desired before unfolding</li>
           <li>Open up bag and crease folds in the proper directions.</li>
         </ol>
@@ -58,6 +81,7 @@ class Bag extends Component {
     )
 
     this.setState({
+      errorMessage: null,
       showBagInstruction: true,
       bagDirections: directions
     })
@@ -68,6 +92,10 @@ class Bag extends Component {
     let instructions = null;
     if (this.state.showBagInstruction) {
       instructions = this.state.bagDirections;
+    }
+    let errorMessage = " ";
+    if (this.state.errorMessage) {
+      errorMessage = this.state.errorMessage
     }
     return (
       <div className="calculator">
@@ -89,10 +117,15 @@ class Bag extends Component {
           onChange={(event) => this.updateValues(event, 'depth')}
           />
         <p
-          onClick={this.calculateBag}
+          onClick={this.checkInputs}
           className="button">
           Calculate
         </p>
+        <a
+          onClick={this.reset}
+          className="clear">clear
+        </a>
+        <p className="error">{errorMessage}</p>
         {instructions}
       </div>
     );
