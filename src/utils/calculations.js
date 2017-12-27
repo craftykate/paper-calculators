e952322f89a72ea262999646f068c791b1ae8c6d
Kate McFaul <katemcfaul@gmail.com>
2017-12-26 20:27:38 -0800

    bold measurements

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -48,8 +48,8 @@ export const calculations = {
         <ol>
           <li>Cut paper <strong>{paperSize.toFixed(2)}{'"'} square.</strong></li>
           <li>Pick any side to be side 1. The opposite side is side 3. The other sides are sides 2 and 4.</li>
-          <li>Punch measurements on sides 1 and 3: {firstPunch.toFixed(2)}</li>
-          <li>Punch measurements on side 2 and 4: {secondPunch.toFixed(2)}</li>
+          <li>Punch measurements on sides 1 and 3: <strong>{firstPunch.toFixed(2)}</strong></li>
+          <li>Punch measurements on side 2 and 4: <strong>{secondPunch.toFixed(2)}</strong></li>
         </ol>
       </div>
     )
@@ -73,8 +73,8 @@ export const calculations = {
         <ol>
           <li>Cut paper {paperSize.toFixed(2)} square</li>
           <li>Pick any side to be side 1. The opposite side is side 3. The other sides are sides 2 and 4.</li>
-          <li>Punch measurements on sides 1 and 3: {firstPunch.toFixed(2)} and {secondPunch.toFixed(2)}</li>
-          <li>Punch measurements on side 2 and 4: {thirdPunch.toFixed(2)} and {fourthPunch.toFixed(2)}</li>
+          <li>Punch measurements on sides 1 and 3: <strong>{firstPunch.toFixed(2)} and {secondPunch.toFixed(2)}</strong></li>
+          <li>Punch measurements on side 2 and 4: <strong>{thirdPunch.toFixed(2)} and {fourthPunch.toFixed(2)}</strong></li>
           <li>Cut bottom liner 1/4{`"`} or 1/8{`"`} smaller than {width.toFixed(2)} by {depth.toFixed(2)}</li>
         </ol>
       </div>
