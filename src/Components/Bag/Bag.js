34029b07db2af26894f489a642a3990f705f388f
Kate McFaul <katemcfaul@gmail.com>
2017-12-26 16:39:14 -0800

    pull calculator out into separate component

--- src/Components/Bag/Bag.js
+++ src/Components/Bag/Bag.js
@@ -8,50 +8,23 @@ class Bag extends Component {
 
   render() {
     this.props.title("Gift Bag");
-    let instructions = null;
-    if (this.props.state.showInstructions) {
-      instructions = (
-        <div className="instructions">
-          <h3 className="instructions">Instructions:</h3>
-          {this.props.state.directions}
-        </div>
-      );
-    }
-    let errorMessage = " ";
-    if (this.props.state.errorMessage) {
-      errorMessage = this.props.state.errorMessage
-    }
+
+    let inputs = [
+      ["height", "Enter bag height (top to bottom)"],
+      ["width", "Enter bag width (left to right)"],
+      ["depth", "Enter bag depth (front to back)"]
+    ]
+
     return (
-      <div className="calculator">
-        <h2>Gift Bag Calculator</h2>
-        <p className="description">Enter all measurements in inches as decimal numbers. Ex: 4.5</p>
-        <p className="inputTitle">Enter bag height (top to bottom):</p>
-        <input
-          value={this.props.state.height}
-          onChange={(event) => this.props.updateValues(event, 'height')}
-          />
-        <p className="inputTitle">Enter bag width (left to right)</p>
-        <input
-          value={this.props.state.width}
-          onChange={(event) => this.props.updateValues(event, 'width')}
-          />
-        <p className="inputTitle">Enter bag depth (front to back)</p>
-        <input
-          value={this.props.state.depth}
-          onChange={(event) => this.props.updateValues(event, 'depth')}
-          />
-        <p
-          onClick={() => this.props.checkInputs('bag')}
-          className="button">
-          Calculate
-        </p>
-        <a
-          onClick={this.props.reset}
-          className="clear">clear
-        </a>
-        <p className="error">{errorMessage}</p>
-        {instructions}
-      </div>
+      <Calculator
+        name="Gift Bag Calculator"
+        state={this.props.state}
+        inputs={inputs}
+        calculatorType='bag'
+        updateValues={this.props.updateValues}
+        checkInputs={this.props.checkInputs}
+        reset={this.props.reset}
+      />
     );
   }
 }
--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,48 +1,51 @@
 import React from 'react';
 import './Calculator.css';
 
-// Pick one:
-// More complex
 const Calculator = (props) => {
-  return (
-      <div className="calculator">
-        <h2>Gift Bag Calculator</h2>
-        <p className="description">Enter all measurements in inches as decimal numbers. Ex: 4.5</p>
-        <p className="inputTitle">Enter bag height (top to bottom):</p>
-        <input
-          value={this.props.height}
-          onChange={(event) => this.props.updateValues(event, 'height')}
-          />
-        <p className="inputTitle">Enter bag width (left to right)</p>
-        <input
-          value={this.props.width}
-          onChange={(event) => this.props.updateValues(event, 'width')}
-          />
-        <p className="inputTitle">Enter bag depth (front to back)</p>
-        <input
-          value={this.props.depth}
-          onChange={(event) => this.props.updateValues(event, 'depth')}
-          />
-        <p
-          onClick={() => this.props.checkInputs('bag')}
-          className="button">
-          Calculate
-        </p>
-        <a
-          onClick={this.props.reset}
-          className="clear">clear
-        </a>
-        <p className="error">{this.props.errorMessage}</p>
-        {this.props.instructions}
+
+  let instructions = null;
+  if (props.state.showInstructions) {
+    instructions = (
+      <div className="instructions">
+        <h3 className="instructions">Instructions:</h3>
+        {props.state.directions}
       </div>
-  );
+    );
+  }
+
+  let errorMessage = " ";
+  if (props.state.errorMessage) {
+    errorMessage = props.state.errorMessage
+  }
+
+  return (
+    <div className="calculator">
+      <h2>{props.name}</h2>
+      <p className="description">Enter all measurements in inches as decimal numbers. Ex: 4.5</p>
+      {props.inputs.map((input, i) => {
+        return (
+          <div key={i}>
+            <p className="inputTitle">{input[1]}:</p>
+            <input
+              value={props.state[input[0]]}
+              onChange={(event) => props.updateValues(event, input[0])}
+              />
+          </div>
+        )
+      })}
+      <p
+        onClick={() => props.checkInputs(props.calculatorType)}
+        className="button">
+        Calculate
+      </p>
+      <a
+        onClick={props.reset}
+        className="clear">clear
+      </a>
+      <p className="error">{errorMessage}</p>
+      {instructions}
+    </div>
+  )
 };
 
 export default Calculator;
-
-// Simple rendering
-// const Calculator = (props) => (
-//   <p>Calculator</p>
-// );
-//
-// export default Calculator;
